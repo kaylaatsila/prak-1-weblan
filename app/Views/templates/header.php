3196c58e54d42f5fd2b9bf9a4cedfe5dc2287754
kaylaatsila <kayla.atsilaivanka2008@students.unila.ac.id>
2022-09-07 00:40:55 +0700

    add bs on header & footer,  add database etc

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,16 +1,13 @@
 <!doctype html>
 <html>
-
 <head>
     <title>Prak. 1 - Web Lanjut - Kayla</title>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
     <style>
-    * {
-        font-family: 'Inter', sans-serif;
-    }
+        *{
+            font-family: 'Inter', sans-serif;
+        }
     </style>
 </head>
-
 <body>
     <h2><?= esc($title) ?></h2>
